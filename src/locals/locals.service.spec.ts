2f3540e2b231ac5382f3921385729d5a8b0b762e
dominicpam89 <dominicpam89@gmail.com>
2024-04-19 16:44:52 +0700

    establish jest:test for locals.service.spec.ts and little adjument to package.json

--- src/locals/locals.service.spec.ts
+++ src/locals/locals.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocalsService } from './locals.service';
+import { Compound } from './entity/compound.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('LocalsService', () => {
   let service: LocalsService;
@@ -14,5 +16,31 @@ describe('LocalsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('return type of Compound[]', async () => {
+    const compounds = await service.getCompounds();
+    expect(compounds).toBeDefined();
+    expect(compounds.length > 0).toBeTruthy();
+    expect(compounds.at(0)).toHaveProperty('pk');
+    expect(compounds.at(0)).toHaveProperty('inci_name');
+    expect(compounds.at(0)).toHaveProperty('smiles');
+  });
+
+  it('getCompound return type of Compound', async () => {
+    const compound = await service.getCompound('1');
+    expect(compound).toBeDefined();
+    expect(compound).toHaveProperty('pk');
+    expect(compound).toHaveProperty('inci_name');
+    expect(compound).toHaveProperty('smiles');
+  });
+
+  it("thrown an error if given id that doesn't exist", async () => {
+    try {
+      const compound = await service.getCompound('0');
+      expect(compound).toBeUndefined();
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+    }
   });
 });
--- src/locals/types/compound.ts
+++
@@ -1,8 +0,0 @@
-export type Compound = {
-  pk: number;
-  trivial_name: string;
-  cas_number: string;
-  inci_name: string;
-  smiles: string;
-  comedogenicity_class: number;
-};
