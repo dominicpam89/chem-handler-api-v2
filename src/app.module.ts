bc843c830dd253b0c1b1c33aaf81c83354bfc6be
dominicpam89 <dominicpam89@gmail.com>
2024-04-19 13:06:46 +0700

    remove compounds and dbsqlite with it, going to establish compounds methods based on cyclic json server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompoundsModule } from './compounds/compounds.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Compound } from './compounds/compounds.entity';
 import { PubchemModule } from './pubchem/pubchem.module';
 import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [Compound],
-      synchronize: false,
-    }),
-    CompoundsModule,
-    PubchemModule,
-  ],
+  imports: [PubchemModule],
   controllers: [AppController],
   providers: [
     AppService,
