6968a8a7debb1e4608303159b4519945d3ab5fc2
dominicpam89 <dominicpam89@gmail.com>
2024-06-02 15:07:21 +0700

    setup enableCors settings as callback in main.ts for handling OPTIONS request and preflight request so server can response with proper CORS information

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,27 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    preflightContinue: false,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    optionsSuccessStatus: 204,
+  });
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE',
+      );
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, X-Requested-With',
+      );
+      return res.sendStatus(204); // Send 204 No Content
+    }
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
