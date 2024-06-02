76e284df6846b847bffcea1dff9efbcd940fc72d
dominicpam89 <dominicpam89@gmail.com>
2024-06-02 15:53:28 +0700

    added logging in main.ts on middleware to know request method and request headers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,10 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     optionsSuccessStatus: 204,
   });
-  app.use((req, res, next) => {
+  app.use((req: Request, res: Response, next: () => void) => {
     if (req.method === 'OPTIONS') {
+      console.log('Received OPTIONS request:', req.headers);
+      res.header('Access-Control-Allow-Origin', '*');
       res.header(
         'Access-Control-Allow-Methods',
         'GET,HEAD,PUT,PATCH,POST,DELETE',
