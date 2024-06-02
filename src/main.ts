f211d18cefb7d7a9041f30a262b743b9df2db42f
dominicpam89 <dominicpam89@gmail.com>
2024-06-02 16:22:28 +0700

    remove middleware for req.method of OPTIONS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     optionsSuccessStatus: 204,
   });
-  app.use((req: Request, res: Response, next: () => void) => {
-    if (req.method === 'OPTIONS') {
-      console.log('Received OPTIONS request:', req.headers);
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE',
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, X-Requested-With',
-      );
-      return res.sendStatus(204); // Send 204 No Content
-    }
-    next();
-  });
   await app.listen(3000);
 }
 bootstrap();
