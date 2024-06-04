9d9562ca113db0b568bddb7a3cea443243d20bbe
dominicpam89 <dominicpam89@gmail.com>
2024-06-04 13:07:27 +0700

    fix getResponse in pubchem.util.ts handling error with HttpExeption

--- src/pubchem/pubchem.util.ts
+++ src/pubchem/pubchem.util.ts
@@ -1,10 +1,11 @@
+import { HttpException } from '@nestjs/common';
+
 export const getResponse = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok)
-    throw new Response(response.body, {
-      status: response.status,
-      headers: response.headers,
-      statusText: response.statusText,
-    });
+    throw new HttpException(
+      "Couldn't get the data. Value as input in POST request in status:never. Or data doesn't exist.",
+      response.status,
+    );
   return response;
 };
